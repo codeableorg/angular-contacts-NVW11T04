82a54631e4747135bbc0019bc12ca856053b4e56
HLena <gordillo.vi20@gmail.com>
2024-05-17 14:03:00 -0500

    feat: Define routes - '': main route - contact/:id : subroute of main route - 404: error route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './shared/ui/components/contact/contact.component';
+import { Error404Component } from './error404/error404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+    children:[
+      {
+        path:'contact/:id',
+        component: ContactComponent
+      }
+    ]
+  },
+  {
+    path:'404',
+    component: Error404Component
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
