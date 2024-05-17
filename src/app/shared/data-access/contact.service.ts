c03256fda474a4a8f0aa065229532f55a1d8f4e7
HLena <gordillo.vi20@gmail.com>
2024-05-17 14:24:07 -0500

    feat: Create contact service

---
+++ src/app/shared/data-access/contact.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { Contact } from '../interface/contact.interface';
+
+@Injectable({providedIn: 'root'})
+export class ContactService {
+  constructor() { }
+
+}
