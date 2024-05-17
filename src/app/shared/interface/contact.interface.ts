7dba897b7bb987c97b090f5e55126354faa058e3
HLena <gordillo.vi20@gmail.com>
2024-05-17 14:23:45 -0500

    feat: Add contact interface

---
+++ src/app/shared/interface/contact.interface.ts
@@ -0,0 +1,9 @@
+ export interface Contact {
+    id: string
+    name: string
+    lastname: string
+    twitter_username: string
+    avatar_url: string
+    favorite: boolean
+    note: string
+ }
